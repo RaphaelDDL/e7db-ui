80a3a0e4e7a4a2006a48c48ba721c43f9df9d134
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-06-13 13:30:17 -0300

    removal of vue-cli in preparation for nuxt project

--- src/js/util/Constants.js
+++
@@ -1,6 +0,0 @@
-import packagejson from '../../../package.json';
-
-export const analyticsId = packagejson.data.analyticsId;
-export const assetsUrl = packagejson.data.assetsEndpoint;
-export const apiUrl = packagejson.data.apiEndpoint;
-export const projectVersion = packagejson.version;
