b263e087cd90b05d2c27d9bc47614d3905f20e8f
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-07-17 18:48:54 -0300

    changing scrollTo from options to x,y

--- plugins/routerAfterEach.js
+++ plugins/routerAfterEach.js
@@ -1,11 +1,12 @@
 export default ({ app }) => {
     app.router.afterEach((/* to, from */) => {
         if ("scrollTo" in window) {
-            window.scrollTo({
-                top: 0,
-                left: 0,
-                // behavior: 'smooth',
-            });
+            window.scrollTo(0, 0);
+            // window.scrollTo({
+            //     top: 0,
+            //     left: 0,
+            //     // behavior: 'smooth',
+            // });
         }
     });
 };
