797260dbe6c9b5d0d034097bdf1e6c24d9bbbbfd
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-07-18 19:26:53 -0300

    changing voice list to single playlist instance for better page scrolling performance

--- components/hero/voices.vue
+++ components/hero/voices.vue
@@ -4,7 +4,19 @@
             <h1>{{ $t("heroes.voice") }}</h1>
             <hr />
             <no-ssr placeholder="Audio files are being rendered..">
-                <ul class="heroes-voice-list">
+                <aplayer
+                    :music="firstMusic"
+                    preload="none"
+                    :mutex="true"
+                    :volume="1"
+                    :autoplay="false"
+                    repeat="no-repeat"
+                    :controls="false"
+                    controlslist="nodownload"
+                    theme="#050a13"
+                    :list="musicList"
+                />
+                <!-- <ul class="heroes-voice-list">
                     <li v-for="voice in voiceList" :key="voice">
                         <aplayer
                             :mini="true"
@@ -22,7 +34,7 @@
                             {{ voice | noUnderscore(true) }}
                         </span>
                     </li>
-                </ul>
+                </ul> -->
             </no-ssr>
         </div>
     </section>
@@ -40,14 +52,37 @@ export default {
             type: String,
         },
     },
+    computed: {
+        firstMusic() {
+            return this.generateMusicObject(this.voiceList[0]);
+        },
+        musicList() {
+            const list = [];
+            this.voiceList.forEach(voice => {
+                list.push(this.generateMusicObject(voice));
+            });
+            list.shift();
+            return list;
+        },
+    },
     methods: {
         generateMusicObject(voice) {
             return {
-                title: voice,
+                title: this.$options.filters.noUnderscore(voice, true),
                 src: `${this.assetsUrl}/voice/${this.id}/${voice}.wav`,
                 pic: `${this.assetsUrl}/hero/${this.id}/icon.png`,
+                artist: " ",
             };
         },
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.aplayer {
+    color: white;
+    background: transparent;
+    box-shadow: none;
+    overflow: visible;
+}
+</style>
